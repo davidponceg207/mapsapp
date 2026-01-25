0be3b7b24f55bab3ba2dcc426ff1c786d382d891
David <davidponceg207@gmail.com>
2026-01-24 18:40:09 -0600

    Request Permission

---
+++ src/actions/permissions/location.ts
@@ -0,0 +1,32 @@
+import { PERMISSIONS, PermissionStatus as RNPermissionStatus, openSettings, request } from "react-native-permissions";
+import type { PermissionStatus } from "../../infrastructure/interfaces/permissions";
+import { Platform } from "react-native";
+
+
+export const requestLocationPermission = async ():Promise<PermissionStatus> => {
+
+    let status: RNPermissionStatus = 'unavailable';
+
+    if(Platform.OS === 'ios') {
+        status = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else if (Platform.OS === 'android') {
+        status = await request( PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION );
+    } else {
+        throw new Error("Unsupported platform");
+    }
+
+    if(status === 'blocked') {
+        await openSettings();
+        // return await checkLocationPermission();
+    }
+
+    const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
+        granted: 'granted',
+        denied: 'denied',
+        blocked: 'blocked',
+        unavailable: 'unavailable',
+        limited: 'limited',
+    };
+
+    return permissionMapper[status] ?? 'unavailable';
+};

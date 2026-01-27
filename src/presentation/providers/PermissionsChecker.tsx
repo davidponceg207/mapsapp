579d67500767f7796d72bb658ade44a0c584c157
David <davidponceg207@gmail.com>
2026-01-26 18:36:43 -0600

    Permissions Checker

---
+++ src/presentation/providers/PermissionsChecker.tsx
@@ -0,0 +1,42 @@
+import { PropsWithChildren, useEffect } from 'react';
+import { AppState } from 'react-native';
+import { usePermissionStore } from '../store/permissions/usePermissionStore';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { RootStackParams } from '../navigation/StackNavigator';
+
+export const PermissionsChecker = ({ children }: PropsWithChildren) => {
+
+    const { locationStatus, checkLocationPermission } = usePermissionStore();
+    const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+    useEffect(() => {
+        if( locationStatus === 'granted' ) {
+            navigation.navigate('MapScreen');
+        } else if ( locationStatus != 'undetermined') {
+            navigation.navigate('PermissionsScreen');
+        }
+    }, [locationStatus]);
+
+    useEffect(() => {
+        checkLocationPermission();
+    }, []);
+
+    useEffect(() => {
+
+        const suscription = AppState.addEventListener('change', (nextAppState) => {
+            if(nextAppState=== 'active') {
+                checkLocationPermission();
+            }
+        });
+
+        return () => {
+            suscription.remove();
+        };
+    }, [])
+
+
+
+    return (
+        <>{children}</>
+    )
+}

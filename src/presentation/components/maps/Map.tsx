b38f45043657636d917993b1265631bb71a116aa
David <davidponceg207@gmail.com>
2026-02-01 20:21:28 -0600

    FAB Component

--- src/presentation/components/maps/Map.tsx
+++ src/presentation/components/maps/Map.tsx
@@ -1,6 +1,7 @@
 import { Platform } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Location } from '../../../infrastructure/interfaces/location';
+import { FAB } from '../ui/FAB';
 
 interface Props {
     showsUserLocation?: boolean;
@@ -31,6 +32,8 @@ export const Map = ({ showsUserLocation = true, initialLocation}: Props) => {
                     image={ require('../../../assets/custom-marker.png') }
                 />
             </MapView>
+
+            <FAB iconName="compass-outline" onPress={() => console.log('Hello')} style={{bottom: 20, right: 20}} />
         </>
     )
 }

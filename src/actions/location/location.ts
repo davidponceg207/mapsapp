334f8df025bcfdefeab91125022f8848a0693c11
David <davidponceg207@gmail.com>
2026-02-01 21:00:57 -0600

    Actions watchCurrentLocation and clearWatchLocation

--- src/actions/location/location.ts
+++ src/actions/location/location.ts
@@ -18,3 +18,23 @@ export const getCurrentLocation = async():Promise<Location> => {
         })
     })
 };
+
+export const watchCurrentLocation = (locationCallback: (loaction: Location) => void):number => {
+
+    return Geolocation.watchPosition(info => (
+
+        locationCallback({
+            latitude: info.coords.latitude,
+            longitude: info.coords.longitude
+        })
+    ), (error) => {
+        throw new Error("Can't get watchPosition");
+        
+    }, {
+        enableHighAccuracy: true
+    });
+};
+
+export const clearWatchLocation = (watchId: number) => {
+    Geolocation.clearWatch(watchId);
+} 
